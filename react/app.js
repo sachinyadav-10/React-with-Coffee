1cacbd1bf78c73e401ab1f44fc1e27c891135255
sachin <sy083328@gmail.com>
2025-01-31 15:10:24 +0530

    here about the dependencies (npm and parcel)

--- react/app.js
+++ react/app.js
@@ -3,3 +3,6 @@ console.log(heading);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+// two type of dependency 1) dev 2) normal
+// pakage.jason is the configuration for npm
+
