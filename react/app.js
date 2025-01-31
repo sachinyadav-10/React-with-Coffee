1d6c767f392371831e63b5482f8d56b6a924973a
sachin <sy083328@gmail.com>
2025-01-31 06:21:01 +0530

    lets go for this crazy journey

---
+++ react/app.js
@@ -0,0 +1,5 @@
+const heading=React.createElement('h1',{id:"first-tag",ccgj:"hgkhg"},"Hello World using React");
+console.log(heading);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
